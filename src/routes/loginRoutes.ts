2556f0294e540956bb69432c52b902c6d9cee3ea
Nutty Nkosi <nkosin361@gmail.com>
2021-04-06 20:30:48 +0200

    first version of server app in TypeScript

---
+++ src/routes/loginRoutes.ts
@@ -0,0 +1,74 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+/* interface RequestWithBody {
+  body: { [key: string]: string | undefined }
+} */
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if(req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not Permitted')
+}
+
+const router = Router();
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method='POST'>
+      <div>
+        <label>Email</label>
+        <input name='email'/>
+      </div>
+      <div>
+        <label>Password</label>
+        <input name='password' type='password'/>
+      </div>
+      <button>submit</button>
+    </form>
+  `)
+});
+
+router.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if(email && password && email === 'test@test.com' && password === '1234'){
+    req.session = { loggedIn: true };
+    res.redirect('/');
+  }else {
+    res.send('Your email or password is invalid');
+  }
+  
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if(req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <h1>You are logged In</h1>
+        <a href='/logout'>Logout</a>
+      </div>
+    `)
+  }else {
+    res.send(`
+      <div>
+        <h1>You are not logged In</h1>
+        <a href='/login'>LogIn</a>
+      </div>
+    `)
+  }
+})
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user')
+})
+
+export { router }
