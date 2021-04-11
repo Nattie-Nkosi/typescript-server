b24670d988a63d10791622c13f0df594264b2acf
Nutty Nkosi <nkosin361@gmail.com>
2021-04-11 22:04:08 +0200

    Refactor, Adding Decorators to handle routes

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -16,26 +16,13 @@ function requireAuth(req: Request, res: Response, next: NextFunction): void {
 
 const router = Router();
 
-router.get('/login', (req: Request, res: Response) => {
-  res.send(`
-    <form method='POST'>
-      <div>
-        <label>Email</label>
-        <input name='email'/>
-      </div>
-      <div>
-        <label>Password</label>
-        <input name='password' type='password'/>
-      </div>
-      <button>submit</button>
-    </form>
-  `)
-});
+
 
 router.post('/login', (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   if(email && password && email === 'test@test.com' && password === '1234'){
+
     req.session = { loggedIn: true };
     res.redirect('/');
   }else {
